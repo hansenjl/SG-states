78aa8707ed29978c95eff7c24d98b49edeb252e3
hansenjl <jenn.leigh.hansen@gmail.com>
2019-03-19 22:03:27 -0700

    Change comment text column to string

--- db/migrate/20190320024053_create_comments.rb
+++ db/migrate/20190320024053_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
        t.integer :user_id
        t.integer :visit_id
-       t.integer :text
+       t.string :text
     end
   end
 end
