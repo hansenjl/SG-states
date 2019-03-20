d6645b133ed378f13d403b08bfb6a86564e841e6
hansenjl <jenn.leigh.hansen@gmail.com>
2019-03-19 21:46:50 -0700

    Change comment to be associated with a visit instead of a state

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :state
+  belongs_to :visit
   belongs_to :user
 end
--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,5 +1,6 @@
 class Visit < ApplicationRecord
   belongs_to :user
   belongs_to :state
+  has_many :comments, dependent: :destroy
 
 end
