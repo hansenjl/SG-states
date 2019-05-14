e30dd2c52655b19edfc24ec7c7f6509e77b8a1ed
hansenjl <jenn.leigh.hansen@gmail.com>
2019-05-14 10:09:36 -0700

    Remove Visit Helpers

--- app/helpers/visit_helper.rb
+++ app/helpers/visit_helper.rb
@@ -1,20 +1,4 @@
 module VisitHelper
-  def visit_header(visit)
-    if params[:user_id]
-      "#{visit.user.username} visits:"
-    elsif params[:state_id]
-      "#{visit.state.name} visits:"
-    else
-      "All Visits:"
-    end
-  end
 
-  def state_select(visit)
-    if !params[:state_id]
-      collection_select(visit, :state_id, State.order_alpha, :id, :name)
-    else
-      visit.state.name
-    end
-  end
 
 end
