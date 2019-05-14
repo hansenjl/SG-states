3318cac44ec7dc12444ca9dfede456099ecc5b46
hansenjl <jenn.leigh.hansen@gmail.com>
2019-05-14 10:53:32 -0700

    Add edit and update actions

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,4 +1,5 @@
 class VisitsController < ApplicationController
+  before_action :if_not_logged_in, only: [:create, :new, :edit, :update]
 
   def new
     @visit = current_user.visits.build
@@ -22,8 +23,25 @@ class VisitsController < ApplicationController
     @visit = Visit.find_by_id(params[:id])
   end
 
+  def edit
+    @visit = Visit.find_by_id(params[:id])
+    redirect_to visits_path if @visit.user != current_user
+  end
+
+  def update
+    @visit = Visit.find_by_id(params[:id])
+    redirect_to visits_path if @visit.user != current_user
+    if @visit.update(visit_params)
+      redirect_to visit_path(@visit)
+    else
+      render 'visits/edit'
+    end
+  end
+
   private
   def visit_params
     params.require(:visit).permit(:state_id, :rating)
   end
+
+
 end
