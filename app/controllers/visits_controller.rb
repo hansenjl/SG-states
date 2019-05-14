18f56a08719cc1e287dc17b22d46fedeaa7d6b19
hansenjl <jenn.leigh.hansen@gmail.com>
2019-05-14 10:09:15 -0700

    Remove nested routes features

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,20 +1,11 @@
 class VisitsController < ApplicationController
 
   def new
-    if params[:state_id]
-      @visit = current_user.visits.build(state_id: params[:state_id])
-    else
-      @visit = current_user.visits.build
-    end
+    @visit = current_user.visits.build
   end
 
   def create
-    if params[:state_id]
-      @visit = current_user.visits.build(visit_params)
-      @visit.state_id = params[:state_id]
-    else
-      @visit = current_user.visits.build(visit_params)
-    end
+    @visit = current_user.visits.build(visit_params)
     if @visit.save
       redirect_to visits_path
     else
@@ -23,13 +14,7 @@ class VisitsController < ApplicationController
   end
 
   def index
-    if params[:user_id] && user = User.find_by_id(params[:user_id])
-      @visits  = user.visits
-    elsif params[:state_id] && state = State.find_by_id(params[:state_id])
-      @visits  = state.visits
-    else
-      @visits = Visit.all
-    end
+    @visits = Visit.all
   end
 
 
